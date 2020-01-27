93da04865a8c0feb34d4367d531856d7abda3de3
Paul Hiemstra <p.h.hiemstra@windesheim.nl>
2020-01-27 15:04:06 +0100

    Updated docs with new tool name

--- pdoc.bash
+++ pdoc.bash
@@ -1,2 +1,2 @@
 # I generate documentation using [pdoc](https://pdoc3.github.io/pdoc/doc/pdoc/index.html#command-line-interface)
-pdoc --html --output-dir doc ReAgent_scripts --force
+pdoc --html --output-dir doc ReAgent_workflow --force
